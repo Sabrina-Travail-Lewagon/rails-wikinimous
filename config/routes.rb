754ca0d2dc54d3774692264fcad0cb482022bf0e
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-05-15 21:14:51 +0200

    CRUD OK, Vues OK, Ajout notification OK

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,19 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
+  # Liste tous les article
+  get 'articles', to: 'articles#index'
+  # Ajout d'un article
+  get 'articles/new', to: 'articles#new'
+  ## Récupération de la saisie
+  post 'articles', to: 'articles#create'
+  # Afficher un article
+  get 'articles/:id', to: 'articles#show', as: :article
+  # Modifier un article
+  get 'articles/:id/edit', to: 'articles#edit', as: :article_edit
+  ## Récupéré la saisie et MAJ données
+  patch 'articles/:id', to: 'articles#update'
+  # supprimer un article
+  delete 'articles/:id', to: 'articles#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+20.times do
+  article = Article.new(
+    title: Faker::Movie.title,
+    content: Faker::Quote.famous_last_words
+  )
+  article.save
+end
